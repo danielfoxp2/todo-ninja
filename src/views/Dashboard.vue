c4c7b7c2de65b21bccf49de1d29a22dd12c766ae
danielfoxp2 <daniel.foxp2@live.com>
2019-01-12 15:20:50 +0000

    playing with grids

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="dashboard">
+        <h1 class="subheading grey--text">Dashboard</h1>
+
+        <v-container class="my-5">
+            <v-layout row wrap>
+                <v-flex xs12 md6>
+                    <v-btn outline block class="primary">1</v-btn>
+                </v-flex>
+                <v-flex xs12 md6>
+                    <v-btn outline block class="primary">2</v-btn>
+                </v-flex>
+            </v-layout>
+        </v-container>
+
+    </div>
+</template>
+
+<script>
+
+export default {
+}
+</script>
---
+++ src/views/Team.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="team">
+        <h1 class="subheading grey--text">Team</h1>
+        <v-container class="my-5">
+        </v-container>
+    </div>
+</template>
+
+<script>
+
+export default {
+}
+</script>
