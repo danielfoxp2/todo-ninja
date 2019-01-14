0cddfd6f0ac93d7f8114d0e38861e1d323aa4a37
danielfoxp2 <daniel.foxp2@live.com>
2019-01-14 19:13:26 +0000

    Create a tooltip with vuetify

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -3,7 +3,25 @@
         <h1 class="subheading grey--text">Dashboard</h1>
 
         <v-container class="my-5">
-            <v-card flat v-for="project in projects"> 
+
+            <v-layout row class="mb-3">
+                <v-tooltip top>
+                    <v-btn small flat color="grey" @click="sortBy('title')" slot="activator">
+                        <v-icon left small>folder</v-icon>
+                        <span class="caption text-lowercase">By project name</span>
+                    </v-btn>
+                    <span>Sort projects by project name </span>
+                </v-tooltip>
+                <v-tooltip top>
+                    <v-btn small flat color="grey" @click="sortBy('person')" slot="activator">
+                        <v-icon left small>person</v-icon>
+                        <span class="caption text-lowercase">By person</span>
+                    </v-btn>
+                    <span>Sort projects by person</span>
+                </v-tooltip>
+            </v-layout>
+
+            <v-card flat v-for="project in projects" :key="project.title"> 
                 <v-layout row wrap :class="`pa-3 project ${project.status}`">
                     <v-flex xs12 md6>
                         <div class="caption grey--text">{{ project.title }}</div>
@@ -36,13 +54,18 @@ export default {
     data() {
         return {
             projects: [
-                { title: 'Design a new blablabla', person: 'The Moda Foca', due: '1st Jan 2019', status: 'ongoing', content: 'Lorem Ipsum dollor sis amet consctetus moda floca.'},
-                { title: 'Design a new blablabla', person: 'The Moda Foca', due: '1st Jan 2019', status: 'complete', content: 'Lorem Ipsum dollor sis amet consctetus moda floca.'},
-                { title: 'Design a new blablabla', person: 'The Moda Foca', due: '1st Jan 2019', status: 'complete', content: 'Lorem Ipsum dollor sis amet consctetus moda floca.'},
-                { title: 'Design a new blablabla', person: 'The Moda Foca', due: '1st Jan 2019', status: 'ongoing', content: 'Lorem Ipsum dollor sis amet consctetus moda floca.'},
- { title: 'Design a new blablabla', person: 'The Moda Foca', due: '1st Jan 2019', status: 'overdue', content: 'Lorem Ipsum dollor sis amet consctetus moda floca.'}
+                { title: 'Design a new blablabla', person: 'Punisher', due: '1st Jan 2019', status: 'ongoing', content: 'Lorem Ipsum dollor sis amet consctetus moda floca.'},
+                { title: 'Nigga this is a taitou', person: 'Wolverine', due: '1st Jan 2019', status: 'complete', content: 'Lorem Ipsum dollor sis amet consctetus moda floca.'},
+                { title: 'Another description', person: 'Ryu', due: '1st Jan 2019', status: 'complete', content: 'Lorem Ipsum dollor sis amet consctetus moda floca.'},
+                { title: 'This one here is the best', person: 'Gouken', due: '1st Jan 2019', status: 'ongoing', content: 'Lorem Ipsum dollor sis amet consctetus moda floca.'},
+ { title: 'I forgot this one, but not anymore', person: 'Yori', due: '1st Jan 2019', status: 'overdue', content: 'Lorem Ipsum dollor sis amet consctetus moda floca.'}
  
             ]
+        }
+    },
+    methods: {
+        sortBy(prop){
+            this.projects.sort((a,b) => a[prop] < b[prop] ? -1 : 1)
         }
     }
 }
