900ac0f9a0ee372a79453740c61ba8d7f0426718
danielfoxp2 <daniel.foxp2@live.com>
2019-01-21 17:01:18 +0000

    Add snackbars

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,5 +1,11 @@
 <template>
     <nav>
+
+        <v-snackbar v-model="snackbar" :timeout="4000" top color="success">
+            <span>Awesome! You added a new project.</span>
+            <v-btn flat color="white" @click="snackbar = !snackbar">Close</v-btn>
+        </v-snackbar>
+
         <v-toolbar flat app class="white">
             <v-toolbar-side-icon class="grey--text" @click="drawer = !drawer"></v-toolbar-side-icon>
             <v-toolbar-title class="text-uppercase grey--text">
@@ -70,7 +76,8 @@ export default {
                 { icon: 'dashboard', text: 'Dashboard', route: '/' },
                 { icon: 'folder', text: 'My Projects', route: '/projects' },
                 { icon: 'person', text: 'Team', route: '/team' }
-            ]
+            ],
+            snackbar: true
         }
     },
     components: { Popup }
